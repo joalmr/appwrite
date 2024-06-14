20e9ca5c0350961edffd7ad53f05afd9b0a6c8a0
joalmr <joalmr@gmail.com>
2024-06-14 12:40:26 -0500

    env example

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logger/web.dart';
 
 Future<void> main() async {
